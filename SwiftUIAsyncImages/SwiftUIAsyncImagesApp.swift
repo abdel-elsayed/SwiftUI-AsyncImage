27c34c344c41196ef16cda0d8241136700cf2e12
abdel-elsayedd <abdelrahman.elsayed@digital-tectonics.com>
2023-01-17 19:22:07 -0500

    Initial Commit

---
+++ SwiftUIAsyncImages/SwiftUIAsyncImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImagesApp.swift
+//  SwiftUIAsyncImages
+//
+//  Created by Abdelrahman Elsayed on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
